d44a513af046ac706ec6fcd66a62f549920e9055
gbox3d <gbox3d@gmail.com>
2020-10-23 08:44:52 +0000

    checkpoint example

--- example/ex02_checkpoint.py
+++ example/ex02_checkpoint.py
@@ -20,13 +20,19 @@ from utils_ai import pil_draw_lib as pdl
 import tensorflow as tf
 
 
+from IPython.display import display
+
+
 from object_detection.utils import label_map_util
 from object_detection.utils import config_util
 from object_detection.utils import visualization_utils as viz_utils
 from object_detection.builders import model_builder
 
 
-print(f'load tf ok {tf.__version__}')
+print("버전: ", tf.__version__)
+print("즉시 실행 모드: ", tf.executing_eagerly())
+print("GPU ", "사용 가능" if tf.config.experimental.list_physical_devices("GPU") else "사용 불가능")
+
 
 print('load module ok')
 
@@ -63,3 +69,26 @@ def get_model_detection_function(model):
 detect_fn = get_model_detection_function(detection_model)
 
 print("load model ok")
+
+
+#%%
+# load image
+image_path = '../test_img/image2.jpg'
+print('load image')
+# numpy형으로 변환
+image_np = np.array(Image.open(image_path))
+
+# print(image_np.shape)
+# print(type(image_np))
+print(f'{image_path} load ok')
+# plt.imshow(image_np)
+display(Image.fromarray(image_np))
+
+#%%
+input_tensor = tf.convert_to_tensor(
+    np.expand_dims(image_np, 0), dtype=tf.float32)
+detections, predictions_dict, shapes = detect_fn(input_tensor)
+
+print(detections['detection_boxes'][0].numpy())
+# print(predictions_dict)
+# %%
